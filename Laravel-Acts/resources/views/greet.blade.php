a0f884aab2dabea6e52f1cb6acd73fd79cd85f0f
CamilGrace <156520346+CamilGrace@users.noreply.github.com>
2025-03-11 01:33:51 +0000

    Created new repo, add old routing activity

--- Laravel-Acts/resources/views/greet.blade.php
+++ Laravel-Acts/resources/views/greet.blade.php
@@ -34,10 +34,9 @@
 </head>
 <body>
     <div class="container">
-        <!-- <h1>Hello, Laravel!</h1>
-        <p>Camil Grace J. Bumatay</p> -->
+        <h1>Hello, Laravel!</h1>
+        <p>Camil Grace J. Bumatay</p> 
         
-        <p>Hello, {{ $name }}</p> 
     </div>
 </body>
 </html>
